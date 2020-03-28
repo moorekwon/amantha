3231d84f666c8d58284adb41b18b2ef43657b10e
Hyojin Kwon <raccoonhj33@gmail.com>
2020-03-28 20:28:53 +0900

    200328 get usernames api

--- app/members/views.py
+++ app/members/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model
 from django.shortcuts import render
 
 # Create your views here.
@@ -9,6 +9,8 @@ from rest_framework.views import APIView
 
 # 로그인 (토큰 가져오거나 생성)
 from members.serializers import UserSerializer
+
+User = get_user_model()
 
 
 class AuthTokenAPIView(APIView):
@@ -27,3 +29,7 @@ class AuthTokenAPIView(APIView):
             'user': UserSerializer(user).data
         }
         return Response(data)
+
+    def get(self, request):
+        usernames = [user.username for user in User.objects.all()]
+        return Response(usernames)
