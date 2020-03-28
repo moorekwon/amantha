c411867fd3f66c462e57fe8bbdf6d9108c5cc503
Hyojin Kwon <raccoonhj33@gmail.com>
2020-03-28 17:21:44 +0900

    200328 completed deployment

--- .config/gunicorn.py
+++ .config/gunicorn.py
@@ -1,5 +1,5 @@
 daemon = False
 chdir = '/srv/amantha/app'
-bind = 'unix:run/amantha.sock'
+bind = 'unix:/run/amantha.sock'
 accesslog = '/var/log/gunicorn/amantha-access.log'
 errorlog = '/var/log/gunicorn/amantha-error.log'
