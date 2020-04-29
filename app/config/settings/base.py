3735e494338c6ba44ad3e0fbbf7816887776a2a8
Hyojin Kwon <raccoonhj33@gmail.com>
2020-04-29 15:54:07 +0900

    200429 refresh and deploy

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -118,6 +118,14 @@ REST_FRAMEWORK = {
     ],
 }
 
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_STORAGE_BUCKET_NAME = SECRETS['AWS_STORAGE_BUCKET_NAME']
+AWS_AUTO_CREATE_BUCKET = True
+AWS_S3_REGION_NAME = 'ap-northeast-2'
+
+DATETIME_FORMAT = '%Y-%m-%d %H:%M'
+USE_L10N = False
+
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
